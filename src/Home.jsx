7aeb8a774d2efa5bf920c89f9ed3b43816a4ddda
Robyn <127926809+Robyn-web@users.noreply.github.com>
2024-05-14 10:45:09 +0100

    removed image from homePage

--- src/Home.jsx
+++ src/Home.jsx
@@ -12,18 +12,7 @@ function Home() {
       <NavBar />
       <h1>Robyn Windsor</h1>
       <h2 className="subHeader">FRONTEND ENGINEER</h2>
-      <Container>
-        <Row>
-          <Col xs={6} md={4}>
-            <Image
-              src="src/assets/rw_photo_edited.jpg"
-              roundedCircle
-              width="170"
-              height="180s"
-            />
-          </Col>
-        </Row>
-      </Container>
+      <Container></Container>
       <section className="introduction">
         As a passionate frontend developer with a particular focus on climate
         tech solutions, I am driven by the opportunity to merge my coding
